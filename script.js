8602b54963ecfc92e5c2b84b6ec0841fae94a55f
Oliwia Kołecka <oliwia0kolecka@gmail.com>
2023-06-05 10:42:51 +0200

    add additional functions

--- script.js
+++ script.js
@@ -85,11 +85,14 @@ document.addEventListener("DOMContentLoaded", () => {
             paras.classList.add("bubble-user");
             messages.appendChild(paras)
             
+            movieInfo(messageValue);
             /*Construct url for Bechdel from input*/
             getBechdel(messageValue);
 
+            isMovieWorthWatching(messageValue);
+
             /*Construct url for Movie Data Base from input*/
-            getMovieDataBaseUrl(messageValue);
+            // getMovieDataBaseUrl(messageValue);
 
             /*clear the text area*/
             messageInput.value = "";
@@ -114,12 +117,14 @@ submitButton.onclick = function(event) {
     paras.classList.add("bubble-user");
     messages.appendChild(paras)
     
-
+    movieInfo(messageValue);
     /*Construct url for Bechdel from input*/
     getBechdel(messageValue);
 
+    isMovieWorthWatching(messageValue);
+
     /*Construct url for Movie Data Base from input*/
-    getMovieDataBaseUrl(messageValue);
+    // getMovieDataBaseUrl(messageValue);
 
     /*clear the text area*/
     messageInput.value = "";
@@ -233,6 +238,161 @@ function getMovieDataBaseUrl () {
         fetch(urlMDB, options)
         .then(response => response.json())
         .then(response => console.log(response))   
+}
+
+/* function to check if the movie is worth watching on the basis of the rating with the use of Movie Data Base API */
+function isMovieWorthWatching(messageValue) {
+    let urlMDB = url2;
+
+    /* convert input to title case */
+    const lowerInputString = messageValue.toLowerCase();
+    const splitInput = lowerInputString.split(' ');
+    const convertedInput = []
+    splitInput.forEach((word, index) => {
+        if (index === 0) {
+            convertedInput.push(word[0].toUpperCase() + word.slice(1))
+        return
+    }
+    if (word.length > 3) {
+        convertedInput.push(word[0].toUpperCase() + word.slice(1))
+    } else {
+        convertedInput.push(word)
+    }
+    })
+    const movieTitle = convertedInput.join(' ')
+
+    /* construct the url */
+    urlMDB += movieTitle.replaceAll(' ', '%20') + '?exact=true&info=base_info&titleType=movie';
+        console.log(urlMDB)
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'f22efb155emshd153236925f83c7p19b8c5jsn378a0a9c82e9',
+                'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+            }
+        };
+        fetch(urlMDB, options)
+        .then(response => response.json())
+        .then(response => {
+            let parasBot = document.createElement("div")
+            parasBot.classList.add("bubble-bot");
+
+            /* response option - the movie is worth watching */
+            let responseWorthWatching = [
+                "Yes! You won't regret it!",
+                "Yes!! This movie is a big slay!",
+                "Hell yeah, it's so good!",
+                "Yes, go watch it now!",
+                "Yes, it's absolutely worth watching!",
+                "Yes, you should watch it!"
+            ];
+
+            /* response option - the movie is not worth watching */
+            let responseNotWorthWatching = [
+                "If you want to bore yourself to death, sure!",
+                "Nah, this movie's weak.",
+                "No, don't do this to yourself.",
+                "No, it's a waste of time like my ex.",
+                "No, it's not worth your time, babe.",
+                "Nah, it's terrible, trust me.",
+            ];
+
+            /* response option - the movie title is incorrect */
+            let responseWrongTtile = [
+                "Babe, that's not even a correct tiltle.", 
+                "I don't know this movie. Are you sure the title's correct?", 
+                "Yeah, nah, wrong title. Try again.",
+                "Woops. I don't think that's correct.",
+                "Umm... that's not how you spell that title."
+            ];
+
+            /*for picking at random*/
+            let randomWorthWatching = responseWorthWatching[Math.floor(Math.random() * responseWorthWatching.length)];
+            let randomNotWorthWatching = responseNotWorthWatching[Math.floor(Math.random() * responseNotWorthWatching.length)];
+            let randomWrongTtile = responseWrongTtile[Math.floor(Math.random() * responseWrongTtile.length)];
+
+            /* conditional for worth watching */
+            try {
+                let rating = response["results"][0]["ratingsSummary"]["aggregateRating"];
+                if (rating > 7) {
+                    console.log(rating);
+                    console.log(response);
+                        parasBot.innerText = randomWorthWatching;
+                        messages.appendChild(parasBot);
+                    } else {
+                        if (rating < 7) {
+                            console.log(rating);
+                            console.log(response);
+                            parasBot.innerText = randomNotWorthWatching;
+                            messages.appendChild(parasBot);
+                }}
+            } catch(err) {
+                parasBot.innerText = randomWrongTtile;
+                messages.appendChild(parasBot);
+            }
+        })
+}
+
+/* function to obtain information about the movie with the use of Movie Data Base API */
+function movieInfo(messageValue) {
+    let urlMDB = url2;
+
+    /* convert input to title case */
+    const lowerInputString = messageValue.toLowerCase();
+    const splitInput = lowerInputString.split(' ');
+    const convertedInput = []
+    splitInput.forEach((word, index) => {
+        if (index === 0) {
+            convertedInput.push(word[0].toUpperCase() + word.slice(1))
+        return
+    }
+    if (word.length > 3) {
+        convertedInput.push(word[0].toUpperCase() + word.slice(1))
+    } else {
+        convertedInput.push(word)
+    }
+    })
+    const titleInput = convertedInput.join(' ')
+
+    /* construct the url */
+    urlMDB += titleInput.replaceAll(' ', '%20') + '?exact=true&info=base_info&titleType=movie';
+        console.log(urlMDB)
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'f22efb155emshd153236925f83c7p19b8c5jsn378a0a9c82e9',
+                'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+            }
+        };
+        fetch(urlMDB, options)
+        .then(response => response.json())
+        .then(response => {
+            let parasBot = document.createElement("div")
+            parasBot.classList.add("bubble-bot");
+
+            try {
+            let title = response["results"][0]["originalTitleText"]["text"];
+            let releaseYear = response["results"][0]["releaseYear"]["year"];
+            let language = response["results"][0]["plot"]["language"]["id"];
+            let rating = response["results"][0]["ratingsSummary"]["aggregateRating"];
+            let genres = response["results"][0]["genres"]["genres"];
+            let genresList = [];
+            genres.forEach(el => {
+                genresList.push(" "+el["text"])
+            });
+            let id = response["results"][0]["id"];
+            let imdbId = id.slice(2, 9);
+            let responseMovieInfo = `Title: ${title}\nRelease year: ${releaseYear}\nLanguage: ${language}\nGenres: ${genresList}\nRating: ${rating}\nId: ${imdbId}`;
+
+            parasBot.innerText = responseMovieInfo;
+            messages.appendChild(parasBot);
+            console.log(response);
+        } catch(err) {
+            parasBot.innerText = "Babe, I don't know this movie, ask someone else.";
+            messages.appendChild(parasBot);
+            console.log(response);
+        }
+        })
 }
 
 /* Always see the latest message */
